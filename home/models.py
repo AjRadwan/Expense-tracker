bf5a6781f12eb01faa996a5641c294e16b0e1088
Anowar <1hafizajr@gmail.com>
2021-08-30 19:47:15 +0600

    Initial commit

---
+++ home/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+TYPE = (
+    ('Positive', 'Positive'),
+    ('Negative', 'Negative')
+)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    income = models.FloatField()
+    balance = models.FloatField(blank=True, null=True)
+    expenses = models.FloatField(default=0)
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    expense_type = models.CharField(max_length=100, choices=TYPE)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.name
